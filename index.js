ebe07fa36bc979a3ed0766e64ac0a8c609c1db23
Yashwanth_kumar_k <104084611+yeshcrik@users.noreply.github.com>
2025-04-25 22:45:41 +0530

    Update index.js
    Signed-off-by: Yashwanth_kumar_k <104084611+yeshcrik@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.get('/', (req, res) => res.send('Hello from Jenkins Pipeline!'));
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
